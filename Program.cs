2f608ff6f7ced1ab5e52b03f1ae1938ee0002094
Justas <justasjanusis2@gmail.com>
2020-07-02 10:31:24 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TechnicalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPyramid(4);
+        }
+
+        private static void PrintPyramid(int height)
+        {
+            if (height == 1)
+            {
+                Console.WriteLine("*");
+                return;
+            }
+            else if (height > 1)
+            {
+                PrintPyramid(height - 1);
+                for (int i = 0; i < height; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Bad input");
+                return;
+            }
+        }
+    }
+}
